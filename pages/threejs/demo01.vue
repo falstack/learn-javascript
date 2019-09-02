fd80f0df4b28e406b41abb51d29ad1cb43962b10
falstack <icesilt@outlook.com>
2019-09-02 12:24:41 +0800

    init: demo3

--- pages/threejs/demo01.vue
+++ pages/threejs/demo01.vue
@@ -33,7 +33,7 @@ export default {
       const renderer = new THREE.WebGLRenderer()
       renderer.setSize(window.innerWidth, window.innerHeight)
       document.getElementById('demo-01').appendChild(renderer.domElement)
-      // 创建一个几何体
+      // 创建一个立方体
       const geometry = new THREE.BoxGeometry(1, 1, 1)
       // 创建一个材质对象
       const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
---
+++ pages/threejs/demo03.vue
@@ -0,0 +1,45 @@
+<template>
+  <canvas id="demo-03"></canvas>
+</template>
+
+<script>
+import * as THREE from 'three'
+
+export default {
+  name: '',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      const scene = new THREE.Scene()
+      const canvas = document.getElementById('demo-03')
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
+      const renderer = new THREE.WebGLRenderer({ canvas })
+      const fov = 75 // 视角的角度（眼镜的开口角度）
+      const aspect = window.innerWidth / window.innerHeight // 大概就是 far / far处的画布宽度 = 2
+      const near = 0.1 // 近处（距离视角 camera 0.1 以下的对不渲染）
+      const far = 5 // 远处（距离视角 camera 5 以上的对不渲染）
+      const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+      camera.position.z = 2
+      const boxWidth = 1
+      const boxHeight = 1
+      const boxDepth = 1
+      const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth)
+      const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 })
+      const cube = new THREE.Mesh(geometry, material)
+      scene.add(cube)
+      renderer.render(scene, camera)
+    }
+  }
+}
+</script>
