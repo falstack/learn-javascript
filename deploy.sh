beda44d19286aaa0ac5883164c9e3aee2d8ce5d6
falstack <icesilt@outlook.com>
2019-09-06 14:22:37 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+# npm run lint
+npm run docs:build
+
+# cd 到构建输出的目录下
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:falstack/learn-javascript.git master:gh-pages
+
+cd -
+git add -A
+git commit -m 'update'
+git push origin master
