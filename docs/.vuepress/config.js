b7a75a4570513f640f2eb397cb3bdc8708f289c6
falstack <icesilt@outlook.com>
2019-09-06 16:32:31 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  base: process.env.NODE_ENV === 'production' ? '/learn-javascript/' : '/'
-  // theme: 'simple',
-  // themeConfig: {
-  //   author: '冰淤'
-  // }
+  base: process.env.NODE_ENV === 'production' ? '/learn-javascript/' : '/',
+  theme: 'simple',
+  themeConfig: {
+    author: '冰淤'
+  }
 }
