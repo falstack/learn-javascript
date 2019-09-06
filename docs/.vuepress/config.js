32a8e5682f0149435822ae517ba96bd6c8531745
falstack <icesilt@outlook.com>
2019-09-06 17:23:35 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   base: process.env.NODE_ENV === 'production' ? '/learn-javascript/' : '/',
+  title: '{ do something }',
+  description: '用简单的言语，表达超载的心',
   theme: 'simple',
   themeConfig: {
     author: '冰淤'
